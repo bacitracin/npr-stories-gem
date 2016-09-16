7ad67d87223af7c53a8658444b6f5cb31309e6b6
bacitracin <tracyctran@gmail.com>
2016-09-15 17:29:54 -0700

    update gemspec with summary, description, homepage

--- lib/npr_stories/scraper.rb
+++ lib/npr_stories/scraper.rb
@@ -1,5 +1,6 @@
 class NprStories::Scraper
 
+# apiKey is not connected to to my account
 BASE_URL = 'http://api.npr.org/query?apiKey=MDIyODk1NDk0MDE0NTYxNzg5NTdkYjVhZA000&id='
 
 #scrape_programs - head over to NPR programs page, scrape data & instantiate new Program objects
@@ -20,16 +21,9 @@ BASE_URL = 'http://api.npr.org/query?apiKey=MDIyODk1NDk0MDE0NTYxNzg5NTdkYjVhZA00
     api_query = "#{BASE_URL + program.search_id.to_s}"
     doc = Nokogiri::XML(open(api_query))
     @program_stories = doc.xpath('*//story') # @progam_stories is a Nokogiri::XML::NodeSet of stories
-    @program_stories.each do |story|
+    @program_stories.each do |nokogiri_story|
       new_story = NprStories::Story.new
-      new_story.story_title = story.search('title').first.text
-      new_story.program = program # add in stoy's relationship to program object
-      new_story.program_title = story.search('program')[0].text
-      new_story.story_date = story.search('storyDate')[0].text
-      new_story.teaser = story.search('teaser')[0].text
-      new_story.story_url = story.search('link')[0].text
-      new_story_topic = story.search('slug')[0].text
-      new_story.save
+      new_story.set_attributes_from_web(nokogiri_story)
       program.stories << new_story # add new story to program's stories array
     end
     program.stories
--- lib/npr_stories/story.rb
+++ lib/npr_stories/story.rb
@@ -11,4 +11,15 @@ class NprStories::Story
     @@stories
   end
 
+  def set_attributes_from_web(nokogiri_story)#nokogiri story object node?? thing
+    self.story_title = nokogiri_story.search('title').first.text
+   # new_story.program = ##### add in stoy's relationship to program object
+    self.program_title = nokogiri_story.search('program')[0].text
+    self.story_date = nokogiri_story.search('storyDate')[0].text
+    self.teaser = nokogiri_story.search('teaser')[0].text
+    self.story_url = nokogiri_story.search('link')[0].text
+    self.save
+  end
+
+
 end
